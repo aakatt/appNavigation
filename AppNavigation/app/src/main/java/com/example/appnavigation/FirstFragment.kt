7f4ae718591a7a1388551b6c67eabf2f9c43c8f2
aakatt <pablocrs2004@gmail.com>
2022-06-11 15:35:36 -0300

    v1

---
+++ AppNavigation/app/src/main/java/com/example/appnavigation/FirstFragment.kt
@@ -0,0 +1,24 @@
+package com.example.appnavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        view.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment)}
+
+        return view
+    }
+}
---
+++ AppNavigation/app/src/main/java/com/example/appnavigation/SecondFragment.java
@@ -0,0 +1,6 @@
+package com.example.appnavigation;
+
+import android.app.Activity;
+
+public class SecondFragment extends Activity {
+}
